79b873f60e36ebe7ca39c4b5a72d31a580ff72f3
Patrick Navega <patricknavega@MacBook-Air-de-Patrick.local>
2022-08-18 22:34:39 -0300

    Configuração database

---
+++ src/config/mysql.js
@@ -0,0 +1,15 @@
+import mysql from "mysql"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+var pool = mysql.createPool({
+    "user": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_PASSWORD,
+    "database": process.env.MYSQL_DATABASE,
+    "host": process.env.MYSQL_HOST,
+    "port" : process.env.MYSQL_PORT
+})
+
+export default pool
